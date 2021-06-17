627a1a74cc2883bacea36be3513f10ba1414c927
Xuchen Sun <55630755+XuchenSun@users.noreply.github.com>
2021-06-17 21:09:23 -0230

    Create leetcode1365.js

---
+++ JS_practice/array/leetcode1365.js
@@ -0,0 +1,26 @@
+/**leetcode 1365
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+// use two for loop to calculate the smaller number
+// use array.push to get the value of smaller number
+var smallerNumbersThanCurrent = function(nums) {
+    let array_of_smaller_number=[];
+    let count=0;
+    
+    for(let i=0;i<nums.length;i++){
+        
+        for(let j=0;j<nums.length;j++){
+           if(nums[j]<nums[i]){count++;}
+        
+        }
+        array_of_smaller_number.push(count);// push count into array one by one when the end of loop of j
+        count=0;// refresh the count as 0
+    }
+ 
+         
+       
+   
+    return array_of_smaller_number;
+    
+};
