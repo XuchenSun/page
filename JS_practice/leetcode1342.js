b7ce6fb8f955155742de537e7029b2f174566158
Xuchen Sun <55630755+xuchensun@users.noreply.github.com>
2021-06-24 16:13:46 -0230

    Create leetcode1342.js

---
+++ JS_practice/leetcode1342.js
@@ -0,0 +1,14 @@
+/**leetcode1342
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let count=0;
+    
+   while(num!==0){
+       num=num%2===0?num/2:num-1;
+       count++
+   }
+    return count;
+    
+};
