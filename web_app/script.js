c6482e6074f2b905e4f20bf68c20d472180c6d86
sunxu <xuchens@mun.ca>
2021-07-23 22:24:14 -0230

    add web app weather

--- web_app/script.js
+++ web_app/script.js
@@ -9,6 +9,7 @@ let weather={
         const {icon,description}=data.weather[0];
         const {temp,humidity}=data.main;
         const { speed}=data.wind;
+
        
         document.querySelector(".city").innerText="Weather in "+ name;
         document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+icon+".png"
@@ -16,7 +17,9 @@ let weather={
         document.querySelector(".temp").innerText=temp+"°C";
         document.querySelector(".humidity").innerText="Humidity: " + humidity+"%";
         document.querySelector(".wind").innerText="Wind Speed: "+ speed + " km/h";
+
         document.querySelector(".weather").classList.remove("loading");
+
         document.body.style.backgroundImage="url('https://source.unsplash.com/user/erondu/1600x900/?"+ name +"')";
     },
     
